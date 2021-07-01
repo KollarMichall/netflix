ddce2ae2d064ffcddcccc14df53a037448f8152b
KollarMichall <kollar.michal21@gmail.com>
2021-07-01 17:33:25 +0200

    newest

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import './navbar.scss'
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="container">
+                <div className="left">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Netflix_logo.svg/800px-Netflix_logo.svg.png" alt="logo"></img>
+                </div>
+                <div className="right"></div>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Navbar
