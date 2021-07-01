4102768f43110f9a5f80412fd0ee3f301b0c5a74
KollarMichall <kollar.michal21@gmail.com>
2021-07-01 17:34:45 +0200

    new

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import Navbar from '../components/navbar/Navbar'
 import './home.scss'
 
 const Home = () => {
     return (
         <div className="home">
-           home 
+           <Navbar/>
         </div>
     )
 }
