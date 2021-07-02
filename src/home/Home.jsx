8d9c0d541282600f5aa7987cdc4f6ce1a24da1b0
KollarMichall <kollar.michal21@gmail.com>
2021-07-02 21:50:51 +0200

    new

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,12 +1,12 @@
 import Navbar from '../components/navbar/Navbar'
+import Featured from '../components/featured/Featured'
 import './home.scss'
 
 const Home = () => {
     return (
         <div className="home">
            <Navbar/>
-           <img width="100%" src="https://pressbooks.com/app/uploads/sites/47296/2015/12/Asian-Girl-iPad.jpg" alt="photo" />
-
+           <Featured/>
            </div>
              )
             }
