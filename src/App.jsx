4f9eb6dfb98d812662249cec628eb6403336486c
Kollar Michall <kollar.michal21.gmail.com>
2021-07-05 21:21:18 +0200

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './app.scss'
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
 import Register from './pages/register/Register';
+import Watch from './pages/watch/Watch';
 
 function App() {
   return (
     <div>
-     <Register/>
+     <Home/>
      
     </div>
   );
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,6 +1,19 @@
+import { useRef } from 'react'
+import { useState } from 'react'
 import './register.scss'
 
 const Register = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const emailRef = useRef()
+    const passwordRef = useRef()
+
+    const handleStart = () => {
+        setEmail(emailRef.current.value)
+    }
+    const handleFinish = () => {
+        setPassword(passwordRef.current.value)
+    }
     return (
         <div className="register">
             <div className="top">
@@ -13,8 +26,20 @@ const Register = () => {
                 <h1>Ultimated movies, TV shows, and more.</h1>
                 <h2>Watch anywhere. Cancel anytime.</h2>
                 <p>Ready to watch? Enter your email to create or restart your membership.</p>
-                <input type="email" placeholder="email address" />
-                <button>Get started</button>
+                {!email ?(
+                <div className="input">
+                <input type="email" placeholder="email address" ref={emailRef} />
+                <button className="registerButton" onClick={handleStart}>Get started</button>
+                </div>
+
+                )
+                : (
+                <form className="input">
+                <input type="password" placeholder="password" ref={passwordRef} />
+                <button className="registerButton" onClick={handleFinish}>Start</button>
+                </form>
+
+                )}
             </div>
 
 
